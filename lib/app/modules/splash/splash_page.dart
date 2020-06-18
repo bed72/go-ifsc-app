7850dd9f106f0360ef5b7dc13e813b023bdad232
bed72 <gjramos100@gmail.com>
2020-06-18 11:30:24 -0300

    :art: database sqllite

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:go_ifsc/app/modules/core/interfaces/local_storage_interface.dart';
+import 'package:go_ifsc/app/modules/core/services/shared_local_storage_service.dart';
 
 class SplashPage extends StatefulWidget {
   @override
@@ -8,12 +11,16 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+  final ILocalStorage storage = SharedLocalStorageService();
+
   String _assets = "assets/animate/ifsc.flr";
   String _animationName = "ping";
 
   @override
   void initState() {
     super.initState();
+    notification();
     Future.delayed(Duration(seconds: 5)).then(
       (value) => Modular.to.pushReplacementNamed('/login'),
     );
@@ -22,6 +29,44 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void dispose() {
     super.dispose();
+  }
+
+  void notification() {
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('\n\nonMessage: ${message.runtimeType}\n\n');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('\n\nonLaunch: ${message.runtimeType}\n\n');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('\n\nonResume: ${message.runtimeType}\n\n');
+      },
+    );
+    _fcm.requestNotificationPermissions(
+      IosNotificationSettings(
+        sound: true,
+        badge: true,
+        alert: true,
+        provisional: true,
+      ),
+    );
+    _fcm.onIosSettingsRegistered.listen(
+      (IosNotificationSettings settings) {
+        print('\n\nConfigurações Registradas: $settings\n\n');
+      },
+    );
+    _fcm.getToken().then(
+      (String token) async {
+        assert(token != null);
+        print('\n\nSEU TOKEN: $token \n\n');
+        changeToken(token);
+      },
+    );
+  }
+
+  Future changeToken(String token) async {
+    await storage.put('tokenFirebase', token);
   }
 
   @override
